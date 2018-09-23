7e645dc73b62c250447bb9979ca0229e6925e3cd
gigatoride <42883904+gigatoride@users.noreply.github.com>
2018-09-24 01:45:23 +0200

    Main package file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**************************************
+ * Detector Methods:                  *
+ * - transfer.detect()                *  
+ * - profane.detect()                 *
+ * - activity.detect()                *
+ * - memo.detect()                    *
+ *************************************/
+
+// Export detector methods
+module.exports = require('./lib/detector');
