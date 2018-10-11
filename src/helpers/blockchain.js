8f4448061abe53dc39b6bdcab68e059cf6f768ff
gigatoride <gigatoride@gmail.com>
2018-10-11 12:21:51 +0200

    Create scan methods for blockchain.

---
+++ src/helpers/blockchain.js
@@ -0,0 +1,182 @@
+const utils = require('../utils'); // Some utils
+
+/**
+ * Blockchain class
+ */
+class Blockchain {
+  /**
+   * @param {object} options - scan class object
+   */
+  constructor(options = {}) {
+    this.options = options; // Options object for dsteem client instance.
+    this.client = this.options.client; // Set client instance.
+    this.stream = this.client.blockchain.getOperationsStream(); // Create new dsteem instance.
+  }
+
+  /**
+   * Pause operations stream
+   */
+  pause() {
+    if (!this.stream.isPaused()) this.stream.pause(); // Pause stream instance if it running.
+  }
+
+  /**
+   * Resume operations stream
+   */
+  resume() {
+    if (this.stream.isPaused()) this.stream.pause(); // Resume stream instance if it not running.
+  }
+
+  /**
+   * Scan for any blacklisted user on blockchain latest blocks.
+   * @param {function} callback - a callback for results.
+   */
+  blacklist(callback) {
+    // Check callback is a valid function
+    if (typeof callback === 'function') {
+      this.stream // Stream blockchain operations
+          .on('data', async (operation) => {
+            let username;
+            const [txType, txData] = operation.op;
+            switch (txType) {
+              case 'transfer': // In case of transfer set username from sender
+                username = txData.from;
+                break;
+              case 'comment': // In case of transfer set username from author
+                username = txData.author;
+                break;
+              default:
+              // The default username should be null
+                username = null;
+                break;
+            }
+            if (username) {
+              const res = await utils.filter.inBlackList(username); // Filter username in global blacklist then return an array of services if exist.
+              if (res.blacklisted.length) callback(null, res); // Callback if blacklisted array elements more than zero.
+            }
+          })
+          .on('error', callback);
+    } else throw new Error('Callback is not a function'); // Throw an error if callback is not a function
+  }
+
+  /**
+   * Scan blockchain transfers
+   * @param {array} senders - an array of usernames for senders
+   * @param {string} minAccount - a string for minimum SBD/STEEM amount
+   * @param {array} receivers - an array of usernames for receivers
+   * @param {string} targetMemo - a string for memo to match with the callbacks of transactions
+   * @param {function} callback - a callback for the results
+   */
+  transfer(senders, minAccount, receivers, targetMemo, callback) {
+    // Check receivers is null or in array
+    if (receivers !== null) {
+      if (!Array.isArray(receivers)) {
+        throw new Error('Receivers are not in array.');
+      } // Check if senders has a valid array.
+      if (receivers !== null && !utils.valid.accountName(receivers)) {
+        throw new Error('Receivers are not valid.');
+      } // Check all receivers
+    }
+    // Check senders is null or in array
+    if (senders !== null) {
+      if (!Array.isArray(senders)) throw new Error('Senders are not in array.'); // Check if senders has a valid array.
+      if (senders !== null && !utils.valid.accountName(senders)) {
+        throw new Error('Senders are not valid.');
+      } // Check all senders
+    }
+
+    if (
+      !typeof parseInt(minAccount) === 'number' &&
+      !/(SBD|STEEM|\|)/.test(minAccount)
+    ) {
+      throw new Error('Target amount is not valid expected 0.000 STEEM|SBD.');
+    } // Check if senders has a valid array.
+    // Check callback is a valid function
+    if (typeof callback === 'function') {
+      this.stream // Stream blockchain operations
+          .on('data', (operation) => {
+            const [txType, txData] = operation.op;
+            if (txType == 'transfer') {
+            // Check if there is no errors before doing anything.
+              const {from, to, amount, memo} = txData; // destructing the matches object into local variables.
+              // Check operation type, Ternary operator for passing null as true instead of array
+              if (
+              !senders
+                ? true
+                : senders.includes(from) && !receivers // Check senders
+                  ? true
+                  : receivers.includes(to) && !targetMemo // Check receivers
+                    ? true
+                    : memo.includes(targetMemo)
+              ) {
+              // Check memo
+                const [targetAmount, currency] = minAccount.split(/\s/); // Split minAccount to array then local variables with value and name of currency
+                let reAmount = new RegExp(currency); // destructing to array then use type of currency (SBD, STEEM, STEEM|SBD) as regular expression.
+                // Check if transfer account is bigger than or equal minAccount
+                if (
+                  reAmount.test(amount) &&
+                parseFloat(targetAmount) <= parseFloat(amount)
+                ) {
+                  callback(null, txData); // Callback transaction data object
+                }
+              }
+            }
+          })
+      // Callback stream error.
+          .on('error', callback);
+    } else throw new Error('Callback is not a function');
+  }
+
+  /**
+   * Scan blockchain comments/posts/replies
+   * @param {string} username - account username
+   * @param {function} callback - a callback for the results for bad word and author as well
+   */
+  profane(username, callback) {
+    if (username !== null && !utils.valid.accountName(username)) {
+      throw new Error('Username is not valid or exist.');
+    } // Check if username is valid
+    // Check callback is a valid function
+    if (typeof callback === 'function') {
+      this.stream // Stream blockchain operations
+          .on('data', (operation) => {
+          // Stream latest blockchain transaction
+            const [txType, txData] = operation.op;
+            // Check if error is null and operation type is comment
+            switch (txType) {
+              case 'comment':
+              // Check type of transaction
+                if (username === null || txData.author === username) {
+                // Check if username is not null
+                  const word = utils.filter.profaneWord(txData.body); // Check word is profane or not
+                  // String means it is a profane anything else will be false.
+                  if (typeof word === 'string') {
+                    const badWord = [word, txData.author];
+                    callback(null, badWord);
+                  }
+                }
+                break;
+              case 'transfer':
+              // Check type of transaction
+                if (username === null || txData.from === username) {
+                // Check if username is not null
+                  const word = utils.filter.profaneWord(txData.memo); // Check word is profane or not
+                  // String means it is a profane anything else will be false.
+                  if (typeof word === 'string') {
+                    const badWord = [word, txData.author];
+                    callback(null, badWord);
+                  }
+                }
+                break;
+              default:
+                break;
+            }
+          })
+      // Callback stream error.
+          .on('error', callback);
+    } else throw new Error('Callback is not a function'); // Throw an error if callback is not a function
+  }
+}
+
+// Exports Blockchain class
+exports.Blockchain = Blockchain;
