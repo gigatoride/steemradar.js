223be55184ce11ff9876724d2c1a82e7b5b389ca
gigatoride <gigatoride@gmail.com>
2018-10-11 21:11:13 +0200

    Add vote method  to blockchain

--- src/helpers/blockchain.js
+++ src/helpers/blockchain.js
@@ -1,5 +1,4 @@
 const utils = require('../utils'); // Some utils
-
 /**
  * Blockchain class
  */
@@ -27,6 +26,27 @@ class Blockchain {
     if (this.stream.isPaused()) this.stream.pause(); // Resume stream instance if it not running.
   }
 
+  /**
+   * Scan usernames for votes or entire blockchain
+   * @param {array} usernames - scanning these usernames vote their votes
+   * @param {number} targetWeight - the minimum vote weight
+   * @param {function} callback - a callback function for votes results
+   */
+  votes(usernames, targetWeight, callback) {
+    if (typeof targetWeight !== 'number') throw new Error('weight is not a valid number');
+    // Check if username is valid
+    if (usernames && !utils.valid.accountName(usernames)) throw new Error('Username is not valid or exist.');
+    if (typeof callback === 'function') {
+      this.stream
+          .on('data', async (operation) => {
+            const [txType, txData] = operation.op;
+            if (!usernames || (usernames.includes(txData.author) && txType === 'vote')) {
+              if (targetWeight <= txData.weight) callback(null, txData);
+            }
+          })
+          .on('error', callback);
+    } else throw new Error('Callback is not a function'); // Throw an error if callback is not a function
+  }
   /**
    * Scan for any blacklisted user on blockchain latest blocks.
    * @param {function} callback - a callback for results.
@@ -67,30 +87,25 @@ class Blockchain {
    * @param {string} targetMemo - a string for memo to match with the callbacks of transactions
    * @param {function} callback - a callback for the results
    */
-  transfer(senders, minAccount, receivers, targetMemo, callback) {
+  transfers(senders, minAccount, receivers, targetMemo, callback) {
     // Check receivers is null or in array
-    if (receivers !== null) {
-      if (!Array.isArray(receivers)) {
-        throw new Error('Receivers are not in array.');
-      } // Check if senders has a valid array.
-      if (receivers !== null && !utils.valid.accountName(receivers)) {
-        throw new Error('Receivers are not valid.');
-      } // Check all receivers
+    if (receivers) {
+      if (!Array.isArray(receivers)) throw new Error('Receivers are not in array.');
+      // Check if senders has a valid array.
+      if (receivers && !utils.valid.accountName(receivers)) throw new Error('Receivers are not valid.');
+      // Check all receivers
     }
     // Check senders is null or in array
-    if (senders !== null) {
+    if (senders) {
       if (!Array.isArray(senders)) throw new Error('Senders are not in array.'); // Check if senders has a valid array.
-      if (senders !== null && !utils.valid.accountName(senders)) {
-        throw new Error('Senders are not valid.');
-      } // Check all senders
+      if (senders && !utils.valid.accountName(senders)) throw new Error('Senders are not valid.');
+      // Check all senders
     }
 
-    if (
-      !typeof parseInt(minAccount) === 'number' &&
-      !/(SBD|STEEM|\|)/.test(minAccount)
-    ) {
+    // Check if senders has a valid array.
+    if (!typeof parseInt(minAccount) === 'number' && !/(SBD|STEEM|\|)/.test(minAccount)) {
       throw new Error('Target amount is not valid expected 0.000 STEEM|SBD.');
-    } // Check if senders has a valid array.
+    }
     // Check callback is a valid function
     if (typeof callback === 'function') {
       this.stream // Stream blockchain operations
@@ -113,10 +128,7 @@ class Blockchain {
                 const [targetAmount, currency] = minAccount.split(/\s/); // Split minAccount to array then local variables with value and name of currency
                 let reAmount = new RegExp(currency); // destructing to array then use type of currency (SBD, STEEM, STEEM|SBD) as regular expression.
                 // Check if transfer account is bigger than or equal minAccount
-                if (
-                  reAmount.test(amount) &&
-                parseFloat(targetAmount) <= parseFloat(amount)
-                ) {
+                if (reAmount.test(amount) && parseFloat(targetAmount) <= parseFloat(amount)) {
                   callback(null, txData); // Callback transaction data object
                 }
               }
@@ -133,9 +145,8 @@ class Blockchain {
    * @param {function} callback - a callback for the results for bad word and author as well
    */
   profane(username, callback) {
-    if (username !== null && !utils.valid.accountName(username)) {
-      throw new Error('Username is not valid or exist.');
-    } // Check if username is valid
+    // Check if username is valid
+    if (usernames && !utils.valid.accountName(username)) throw new Error('Username is not valid or exist.');
     // Check callback is a valid function
     if (typeof callback === 'function') {
       this.stream // Stream blockchain operations
@@ -146,7 +157,7 @@ class Blockchain {
             switch (txType) {
               case 'comment':
               // Check type of transaction
-                if (username === null || txData.author === username) {
+                if (!username || txData.author === username) {
                 // Check if username is not null
                   const word = utils.filter.profaneWord(txData.body); // Check word is profane or not
                   // String means it is a profane anything else will be false.
@@ -158,7 +169,7 @@ class Blockchain {
                 break;
               case 'transfer':
               // Check type of transaction
-                if (username === null || txData.from === username) {
+                if (!username || txData.from === username) {
                 // Check if username is not null
                   const word = utils.filter.profaneWord(txData.memo); // Check word is profane or not
                   // String means it is a profane anything else will be false.
