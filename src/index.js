795d51d818853ffe6ff13454ca60230a9518d627
gigatoride <gigatoride@gmail.com>
2018-10-11 12:22:42 +0200

    Update index, Add utils for export.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-/**************************************
- * Detector Methods:                  *
- * - transfer.detect()                *  
- * - profane.detect()                 *
- * - activity.detect()                *
- * - memo.detect()                    *
- *************************************/
-
-// Export detector methods
-module.exports = require('./lib/detector');
+// Exports utils
+exports.utils = require('./utils');
+// Exports scan client
+module.exports = require('./scan');
