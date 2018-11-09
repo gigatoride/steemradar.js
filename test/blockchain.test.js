fe07629bce5acfec3cf8f07a2bb22177f9823cf8
gigatoride <gigatoride@gmail.com>
2018-11-09 17:24:28 +0200

    Remove database test.

--- test/blockchain.test.js
+++ test/blockchain.test.js
@@ -37,3 +37,21 @@ test('Detect latest blockchain votes', done => {
     done();
   });
 });
+
+test('Detect any changes for accounts', done => {
+  const usernames = ['utopian-io', 'blocktrades'];
+
+  scan.blockchain.accounts(usernames, err => {
+    expect(err).toBeNull();
+    done();
+  });
+});
+
+test('Detect the database memo has been received by username', done => {
+  const username = 'blocktrades';
+  scan.blockchain.memo(username, err => {
+    scan.pause();
+    expect(err).toBeNull();
+    done();
+  });
+});
