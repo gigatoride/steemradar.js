303f5ce275e5231678e94cc7862f9e16f1c3ece3
gigatoride <gigatoride@gmail.com>
2018-11-09 17:35:11 +0200

    Prefix all unused declarations with _ in tool

--- bin/tool.js
+++ bin/tool.js
@@ -13,96 +13,96 @@ const questions = [
     name: 'method',
     message: 'What do you want to detect?',
     choices: ['Transfers', 'Profane', 'Last time user was active', 'Memos received'],
-    filter: (val) => val.toLowerCase(), // Convert values to lowercase
+    filter: val => val.toLowerCase() // Convert values to lowercase
   },
   {
     type: 'confirm', // Ask a question for true or false
     name: 'blockchain',
     message: 'Ok... Do you want to monitor all blockchain?',
-    when: (answers) => answers.method == 'profane', // If user choice profane
+    when: answers => answers.method === 'profane' // If user choice profane
   },
   {
     type: 'input', // Make an input for the user
     name: 'username',
     message: 'What is your username?',
     // If blockchain answer is false and the choice is profane, last time user was active, memos receives.
-    when: (answers) =>
-      (!answers.blockchain && answers.method == 'profane') ||
-      answers.method == 'last time user was active' ||
-      answers.method == 'memos received',
-    validate: (name) => /[\w---.]{3,}/.test(name), // If blockchain == false and memos received as choice
+    when: answers =>
+      (!answers.blockchain && answers.method === 'profane') ||
+      answers.method === 'last time user was active' ||
+      answers.method === 'memos received',
+    validate: name => /[\w---.]{3,}/.test(name) // If blockchain == false and memos received as choice
   },
   {
     type: 'confirm', // Ask a question for true or false
     name: 'send',
     message: 'Do you want to enter senders?',
     default: true, // Default value for confirm  by enter
-    when: (answers) => answers.method == 'transfers', // If choice is transfers
+    when: answers => answers.method === 'transfers' // If choice is transfers
   },
   {
     type: 'input', // Make an input for the user
     name: 'senders',
     message: 'Please enter senders usernames (user1,user2):',
-    when: (answers) => answers.send,
-    validate: (names) => /[\w--,-.]{3,}/.test(names), // Is valid username(s) not less than 3 letters and numbers, alphabet, dash, comma
-    filter: (usernames) => usernames.split(','), // Convert usernames input to an array
+    when: answers => answers.send,
+    validate: names => /[\w--,-.]{3,}/.test(names), // Is valid username(s) not less than 3 letters and numbers, alphabet, dash, comma
+    filter: usernames => usernames.split(',') // Convert usernames input to an array
   },
   {
     type: 'confirm', // Ask a question for true or false
     name: 'receive',
     message: 'Do you want to enter receivers?',
     default: true, // Default value for confirm  by enter
-    when: (answers) => answers.method == 'transfers', // If choice is transfers
+    when: answers => answers.method === 'transfers' // If choice is transfers
   },
   {
     type: 'input', // Make an input for the user
     name: 'receivers',
     message: 'Please enter receivers (user1,user2)',
-    when: (answers) => answers.receive, // In case of receive is true
-    validate: (names) => /[\w--,-.]{3,}/.test(names), // Is valid username(s) not less than 3 letters and numbers, alphabet, dash, comma
-    filter: (usernames) => usernames.split(','), // Convert usernames input to an array
+    when: answers => answers.receive, // In case of receive is true
+    validate: names => /[\w--,-.]{3,}/.test(names), // Is valid username(s) not less than 3 letters and numbers, alphabet, dash, comma
+    filter: usernames => usernames.split(',') // Convert usernames input to an array
   },
   {
     type: 'list', // Make a list for the user to choice from
     name: 'coin',
     message: 'Choose SBD, STEEM or Both:',
     choices: ['SBD', 'STEEM', 'Both'],
-    filter: (coin) => {
-      if (coin == 'Both') return 'SBD|STEEM'; // Return string for both currency names to match with transfer amount
+    filter: coin => {
+      if (coin === 'Both') return 'SBD|STEEM'; // Return string for both currency names to match with transfer amount
     },
-    when: (answers) => answers.method == 'transfers', // If choice is transfers
+    when: answers => answers.method === 'transfers' // If choice is transfers
   },
   {
     type: 'input', // Make an input for the user
     name: 'amount',
     message: 'Minimum amount to detect (0.001)',
     default: '1000',
-    when: (answers) => answers.method == 'transfers', // If choice is transfers
-    validate: (amount) => {
+    when: answers => answers.method === 'transfers', // If choice is transfers
+    validate: amount => {
       // Make sure it is a valid number
-      if (typeof parseFloat(amount) == 'number') return true;
+      if (typeof parseFloat(amount) === 'number') return true;
       // Return true
       else return 'Please enter a valid amount'; // If amount is not valid
     },
-    filter: (amount) => parseFloat(amount).toFixed(3), // Make sure there is at least three decimal numbers.
+    filter: amount => parseFloat(amount).toFixed(3) // Make sure there is at least three decimal numbers.
   },
   {
     type: 'confirm', // Ask a question for true or false
     name: 'isMemo',
     message: 'Do you want to catch a memo?',
     default: true,
-    when: (answers) => answers.method == 'transfers', // If choice is transfers
+    when: answers => answers.method === 'transfers' // If choice is transfers
   },
   {
     type: 'input', // Make an input for the user
     name: 'memo',
     message: 'Well. Please enter a memo:',
-    when: (answers) => answers.isMemo, // If isMemo is true
-  },
+    when: answers => answers.isMemo // If isMemo is true
+  }
 ];
 
 // How everything gonna works
-inquirer.prompt(questions).then((answers) => {
+inquirer.prompt(questions).then(answers => {
   JSON.stringify(answers, null, '  '); // JSON stringify to object
   switch (
     answers.method // Switch statement for the user choice
@@ -113,14 +113,14 @@ inquirer.prompt(questions).then((answers) => {
         answers.amount + ' ' + answers.coin,
         answers.receive ? answers.receivers : null,
         !answers.isMemo ? null : answers.memo,
-        (transfer) => {
+        transfer => {
           // Start monitoring blockchain by user inputs
           let op = transfer.operations[0][1]; // Operations object and the information about the transfer
           let {
             from, // Sender
             to, // Receivers
             amount, // Amount of crypto
-            memo, // Memo
+            memo // Memo
           } = op; // Convert to local variables
           // If memo is empty
           if (memo === '') {
@@ -132,9 +132,11 @@ inquirer.prompt(questions).then((answers) => {
                 ${chalk.yellowBright(amount)}`);
           } else {
             log(
-                `Transfer from  ${chalk.cyanBright(from)} to ${chalk.cyan(to)}  with an amount of ${chalk.yellowBright(
-                    amount
-                )} memo: ${chalk.magentaBright(memo)}`
+              `Transfer from  ${chalk.cyanBright(from)} to ${chalk.cyan(
+                to
+              )}  with an amount of ${chalk.yellowBright(amount)} memo: ${chalk.magentaBright(
+                memo
+              )}`
             );
           } // Log it
         }
@@ -146,24 +148,24 @@ inquirer.prompt(questions).then((answers) => {
       if (answers.blockchain === true) username = null;
       // Scan in all blockchain
       else username = answers.username; // Scan by username
-      scan.blockchain.profane(username, (err, detected) => {
+      scan.blockchain.profane(username, (_err, detected) => {
         // Start monitoring blockchain by user inputs
         let [word, author] = detected; // Convert object into local variables.
         log(`A profane has been detected: ${chalk.red(word)} Said: ${chalk.cyanBright(author)}`);
       });
       break;
     case 'last time user was active': // In case of transfers
-      scan.database.accounts([answers.username], (err, res) => {
+      scan.database.accounts([answers.username], (_err, res) => {
         // Start monitoring blockchain by user inputs
         log(
-            `@${chalk.cyanBright(answers.username)} last activity: ${chalk.yellowBright(
-                dateFormat(res[0].last_bandwidth_update, 'dddd, mmmm dS, yyyy, h:MM:ss TT')
-            )}`
+          `@${chalk.cyanBright(answers.username)} last activity: ${chalk.yellowBright(
+            dateFormat(res[0].last_bandwidth_update, 'dddd, mmmm dS, yyyy, h:MM:ss TT')
+          )}`
         ); // Log it
       });
       break;
     case 'memos received': // In case of `memos received`
-      scan.database.memo(answers.username, (err, msg) => {
+      scan.database.memo(answers.username, (_err, msg) => {
         // Start monitoring blockchain by user inputs
         if (Array.isArray(msg)) {
           let [from, amount, memo] = msg; // Convert object into local variables.
@@ -172,7 +174,7 @@ inquirer.prompt(questions).then((answers) => {
             log(`${chalk.cyanBright(from)} sent you a memo: ${chalk.magentaBright(memo)}`); // Log it & dont view that tiny amount
           } else {
             log(`${chalk.cyanBright(from)} sent you a memo: ${chalk.magentaBright(
-                memo
+              memo
             )} with an amount of ${chalk.yellowBright(amount)}
             `); // Log it
           }
