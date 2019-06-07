9b17e81cd6600e71a98232bbfacf1c90426f7ff6
gigatoride <gigatoride@gmail.com>
2019-06-07 04:44:45 +0200

    Update library blockchain testing (no exceptions)

--- test/blockchain.test.js
+++ test/blockchain.test.js
@@ -4,61 +4,115 @@ const scan = new steemradar.Scan();
 
 jest.setTimeout(90000);
 
-test('Detect a transfer that has an amount greater than or equal 0.001 SBD/STEEM', done => {
+test('Detect any transfer SBD or STEEM', done => {
   let senders = null;
   let receivers = null;
   let minAmount = '0.001 SBD|STEEM';
   let targetMemo = null;
 
-  scan.blockchain
-    .transfers(senders, minAmount, receivers, targetMemo)
-    .on('data', res => {
-      expect(parseFloat(res.amount)).toBeGreaterThanOrEqual(0.001);
-      done();
-    });
-});
+  const stream = scan.blockchain.transfers(senders, receivers, targetMemo, {
+    minAmount
+  });
 
-// test('Detect public profane word', done => {
-//   scan.blockchain.profanity(null).on('data', res => {
-//     expect(Array.isArray(res)).toBeTruthy();
-//     done();
-//   });
-// });
-
-test('Detect global blacklisted username', done => {
-  scan.blockchain.blacklist().on('data', res => {
-    expect(res.blacklisted.length).toBeTruthy();
+  stream.on('data', trx => {
+    stream.pause();
+    stream.destroy();
     done();
   });
 });
 
 test('Detect latest blockchain votes', done => {
-  scan.blockchain.votes(null, 0).on('data', res => {
-    expect(res).toBeDefined;
+  const stream = scan.blockchain.votes(null, 0);
+  stream.on('data', trx => {
+    stream.pause();
+    stream.destroy();
+    done();
+  });
+});
+
+test('Detect latest account count', done => {
+  const stream = scan.blockchain.accountCounter();
+  stream.on('data', trx => {
+    stream.pause();
+    stream.destroy();
+    done();
+  });
+});
+
+test('Detect global blacklisted accounts', done => {
+  const stream = scan.blockchain.blacklist();
+  stream.on('data', trx => {
+    stream.pause();
+    stream.destroy();
     done();
   });
 });
 
 test('Detect any changes for accounts', done => {
-  const usernames = ['utopian-io', 'blocktrades', 'steemmonsters'];
+  const accounts = [
+    'utopian-io',
+    'blocktrades',
+    'steemmonsters',
+    'deepcrypto8',
+    'ned',
+    'dtube',
+    'steemcleaners',
+    'binance-hot'
+  ];
 
-  scan.blockchain.accounts(usernames).on('data', res => {
-    expect(res).toBeDefined;
+  const stream = scan.blockchain.accountActivity(accounts);
+  stream.on('data', trx => {
+    stream.pause();
+    stream.destroy();
     done();
   });
 });
 
-test('Detect the database memo has been received by username', done => {
-  const username = 'steemmonsters';
-  scan.blockchain.memo(username).on('data', res => {
-    expect(res).toBeDefined;
+test('Detect any mention for any account', done => {
+  const accounts = null;
+
+  const stream = scan.blockchain.accountMentions(accounts);
+  stream.on('data', trx => {
+    stream.pause();
+    stream.destroy();
     done();
   });
 });
 
-test('Tracking funds by a username', done => {
+test('Detect accounts security changes', done => {
+  const accounts = ['utopian-io', 'blocktrades', 'steemmonsters', 'binance-hot', 'ned', 'dtube', 'steemcleaners'];
+
+  const stream = scan.blockchain.accountSecurity(accounts);
+  stream.on('data', trx => {
+    stream.pause();
+    stream.destroy();
+    done();
+  });
+});
+
+test('Detect feed publish prices', done => {
+  const stream = scan.blockchain.feedPublish();
+  stream.on('data', trx => {
+    stream.pause();
+    stream.destroy();
+    done();
+  });
+});
+
+test('Detect public profane word', done => {
+  const stream = scan.blockchain.profanity();
+
+  stream.on('data', res => {
+    stream.pause();
+    stream.destroy();
+    done();
+  });
+});
+
+test('Detect funds movement on blockchain', done => {
   const username = 'gigatoride';
-  scan.blockchain.fundsTracker(username).on('data', res => {
+  const stream = scan.blockchain.fundsTracker(username);
+  stream.on('data', res => {
     expect(res).toBeDefined;
     done();
   });
