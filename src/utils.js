72cc7877461d65e2cd547ada6d8db9dab33376d3
gigatoride <gigatoride@gmail.com>
2019-06-13 18:53:24 +0200

    Readable stream utility, isProfane improvement

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
-const axios = require('axios');
-const Profane = require('bad-words');
-const badWord = new Profane();
+const http = require('http');
+const profanities = require('profanities');
+const { validateAccountName } = require('steem').utils;
+const config = require('./../config.json');
+const { Readable } = require('stream');
 
 /** @private */
 module.exports = {
@@ -28,24 +30,72 @@ module.exports = {
    * Match profane word in broadcast content
    * @param {String} broadcast - Steem broadcast content
    */
-  isProfane: broadcast => {
-    const word = badWord.isProfane(broadcast);
-    return !word ? false : word;
+  isProfane: body => {
+    return body.split(/\s/gm).every(word => {
+      return profanities.includes(word);
+    });
   },
 
   /**
-   * Match global blacklisted usernames
-   * @param {String} username - Steem username
-   * @return {Object} - blacklisted usernames
+   * Match global blacklisted names
+   * @param {String} name - Steem account name
+   * @return {Object} - blacklisted names
    */
-  isBlacklisted: username => {
-    return axios
-      .get('http://blacklist.usesteem.com' + '/user/' + username)
-      .then(res => {
-        return { username: res.data.user, blacklisted: res.data.blacklisted };
-      })
-      .catch(err => {
-        return err;
-      });
+  isBlacklisted: name => {
+    return new Promise((resolve, reject) => {
+      http.get(
+        {
+          hostname: config.global_blacklist_api,
+          port: 80,
+          path: `/user/${name}`,
+          agent: false // create a new agent just for this one request
+        },
+        res => {
+          res.on('data', resolve);
+          res.on('error', reject);
+        }
+      );
+    });
+  },
+  /**
+   * Account names validator
+   * @param {Array} accountNames
+   */
+  isValidAccountNames: accountNames => {
+    return Array.isArray(accountNames) ? !accountNames.every(name => validateAccountName(name) === null) : false;
+  },
+  /**
+   * Creates a readable stream
+   */
+  readableStream: (iterable, opts) => {
+    let iterator;
+    if (iterable && iterable[Symbol.asyncIterator]) iterator = iterable[Symbol.asyncIterator]();
+    else if (iterable && iterable[Symbol.iterator]) iterator = iterable[Symbol.iterator]();
+    else throw new Error(iterable);
+
+    const readable = new Readable({
+      objectMode: true,
+      ...opts
+    });
+    // Reading boolean to protect against _read
+    // being called before last iteration completion.
+    let reading = false;
+    readable._read = function() {
+      if (!reading) {
+        reading = true;
+        next();
+      }
+    };
+    async function next() {
+      try {
+        const { value, done } = await iterator.next();
+        if (done) readable.push(null);
+        else if (readable.push(await value)) next();
+        else reading = false;
+      } catch (err) {
+        readable.destroy(err);
+      }
+    }
+    return readable;
   }
 };
